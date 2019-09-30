d1bb22a656cee6a66a7ee363d619f1455cd3ff56
Bruno Nery <bpnery13@gmail.com>
2019-09-30 22:18:32 +0100

    add model and controller

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,42 @@
+class PlayersController < ApplicationController
+
+  def index
+    @player = Player.all
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    @player.save
+    redirect_to player_path(@player)
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    @player.update(player_params)
+    redirect_to player_path(@player)
+  end
+
+  def destroy
+    @player = Player.find(params[:id])
+    @player.destroy
+    redirect_to players_path
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :last_name, :age, :position, :photo)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+player1 = Player.create({name: 'Bruno', last_name: 'Nery', :age 30, position: 'goalkeeper'})
